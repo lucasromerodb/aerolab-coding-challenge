b0cf8a8d351608fcd168c0146945c5c987a7f5f5
Lucas Romero Di Benedetto <lucasromerodb@gmail.com>
2019-07-02 01:08:44 -0300

    build api requests and test basic implementation

--- src/App.js
+++ src/App.js
@@ -1,27 +1,66 @@
 import React, { useState, useEffect } from "react";
-import { baseUrl, headers } from "./api";
+import { getProducts, getUserMe, getUserHistory, postPoints, postRedeem } from "./api";
 import "./App.css";
 
 function App() {
   const [products, setProducts] = useState([]);
-  const [errors, setErrors] = useState({});
-
-  async function fetchProducts(method = "GET") {
-    const res = await fetch(`${baseUrl}/products`, { method, headers });
-    res
-      .json()
-      .then(products => setProducts(products))
-      .catch(err => setErrors(err));
-  }
+  const [userMe, setUserMe] = useState({});
+  const [userHistory, setUserHistory] = useState([]);
+  const [pointsMsg, setPointsMsg] = useState({});
+  const [redeemMsg, setRedeemMsg] = useState({});
 
   useEffect(() => {
-    fetchProducts();
-  }, []);
+    getProducts(setProducts);
+    getUserMe(setUserMe);
+    getUserHistory(setUserHistory);
+  }, [pointsMsg, redeemMsg]);
+
+  function addPoints() {
+    postPoints(setPointsMsg, 1000);
+  }
+
+  function redeemProduct(productId) {
+    postRedeem(setRedeemMsg, productId);
+  }
 
   return (
     <div className="App">
-      <h1>Challenge</h1>
-      <ul>{products.length ? products.map(p => <li key={p._id}>{p.name}</li>) : ""} </ul>
+      <h1>User</h1>
+      <h2>{userMe.name}</h2>
+      <h3>{userMe.points}</h3>
+      <h3>{userMe._id}</h3>
+      <h2>Points</h2>
+      <button onClick={addPoints}>Add More Points</button>
+      <p>
+        {Object.keys(pointsMsg).length && <span>{pointsMsg.message}</span>}
+        <br />
+        {Object.keys(pointsMsg).length && <span>{pointsMsg["New Points"]}</span>}
+      </p>
+      <h2>Redeem Product</h2>
+      <button onClick={() => redeemProduct("5a0b35d7734d1d08bf7084c9")}>Redeem Switch</button>
+      <button onClick={() => redeemProduct("5a0b3648734d1d08bf708502")}>Redeem 3DS</button>
+      <h2>User History</h2>
+      <ul>
+        {userHistory.length ? (
+          userHistory.map(i => (
+            <li key={i._id + i.createDate}>
+              {i.name} ({i.cost})
+            </li>
+          ))
+        ) : (
+          <li>No hay compras aún...</li>
+        )}
+      </ul>
+      <h2>Products List</h2>
+      <ul>
+        {products.length
+          ? products.map(p => (
+              <li key={p._id}>
+                {p.name} - ({p._id})
+              </li>
+            ))
+          : ""}{" "}
+      </ul>
     </div>
   );
 }
--- src/api.js
+++ src/api.js
@@ -1,12 +1,34 @@
-const baseUrl = "https://coding-challenge-api.aerolab.co";
+const BASE_URL = "https://coding-challenge-api.aerolab.co";
 
-const token =
+const TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZDFhYTYzYTAxYmIyMzAwNzQyMjhlY2IiLCJpYXQiOjE1NjIwMjc1Nzh9.U1iRTO2UvcW61_7rCHjTxWlniYt3YwTxQXsBBqtA6Wc";
 
-const headers = {
+const HEADERS = {
+  crossDomain: true,
   "Content-Type": "application/json",
   Accept: "application/json",
-  Authorization: `Bearer ${token}`
+  Authorization: `Bearer ${TOKEN}`
 };
 
-export { baseUrl, headers };
+async function fetchAPI(url, setState, method = "GET", extra = {}) {
+  const options = { method, headers: HEADERS, ...extra };
+  console.warn(options);
+  return await fetch(`${BASE_URL}${url}`, options)
+    .then(res => res.json())
+    .then(data => setState(data))
+    .catch(err => err);
+}
+
+/* GET */
+const getProducts = setState => fetchAPI("/products", setState);
+const getUserMe = setState => fetchAPI("/user/me", setState);
+const getUserHistory = setState => fetchAPI("/user/history", setState);
+
+/* POST */
+const postPoints = (setState, amount) =>
+  fetchAPI("/user/points", setState, "POST", { body: JSON.stringify({ amount }) });
+
+const postRedeem = (setState, productId) =>
+  fetchAPI("/redeem", setState, "POST", { body: JSON.stringify({ productId }) });
+
+export { getProducts, getUserMe, getUserHistory, postPoints, postRedeem };
