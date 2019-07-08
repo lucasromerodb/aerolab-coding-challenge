8ed15ac71b7826b939ccd2a0394f961008f4e855
Lucas Romero Di Benedetto <lucasromerodb@gmail.com>
2019-07-08 19:21:10 -0300

    Updates

---
+++ precache-manifest.0a5ad0715e5fcd46d2d1e0adccb28a24.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0bae099be7139964be484e7eef96665",
+    "url": "/aerolab-coding-challenge/index.html"
+  },
+  {
+    "revision": "7dada3a2c9954364f853",
+    "url": "/aerolab-coding-challenge/static/css/main.98cc1aaa.chunk.css"
+  },
+  {
+    "revision": "80e106aa3882f72edac3",
+    "url": "/aerolab-coding-challenge/static/js/2.84437a79.chunk.js"
+  },
+  {
+    "revision": "7dada3a2c9954364f853",
+    "url": "/aerolab-coding-challenge/static/js/main.3faab018.chunk.js"
+  },
+  {
+    "revision": "10c35e51c216f3b5304b",
+    "url": "/aerolab-coding-challenge/static/js/runtime~main.6a6b5940.js"
+  },
+  {
+    "revision": "c6da2094219e2ef69660363809d6e9b7",
+    "url": "/aerolab-coding-challenge/static/media/aerolab-logo.c6da2094.svg"
+  },
+  {
+    "revision": "c01e2789062ade1ed7245ef859fe2591",
+    "url": "/aerolab-coding-challenge/static/media/aeropay-logo.c01e2789.svg"
+  },
+  {
+    "revision": "be33dff900159a4fd3e8f89db360aa96",
+    "url": "/aerolab-coding-challenge/static/media/aeropay-pattern.be33dff9.svg"
+  },
+  {
+    "revision": "b763f02eeb6a83844bc8daf931eacc8a",
+    "url": "/aerolab-coding-challenge/static/media/coin.b763f02e.svg"
+  },
+  {
+    "revision": "5221c3be9c7209d688a4f873f23d98b4",
+    "url": "/aerolab-coding-challenge/static/media/featured.5221c3be.jpg"
+  },
+  {
+    "revision": "c408d04d61c234fb22a78bf08c2fc5e1",
+    "url": "/aerolab-coding-challenge/static/media/mastercard.c408d04d.svg"
+  },
+  {
+    "revision": "66544fb30b7cfe52be3966b63ec99f33",
+    "url": "/aerolab-coding-challenge/static/media/pattern.66544fb3.svg"
+  }
+]);
