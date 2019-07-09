5001cd33ed96ca1e84080df8a441af3dcfeba958
Lucas Romero Di Benedetto <lucasromerodb@gmail.com>
2019-07-09 17:29:55 -0300

    improve click outside buy points

--- src/components/buypoints/BuyPoints.js
+++ src/components/buypoints/BuyPoints.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import AeroPay from "../aeropay";
 import { Button } from "../../styles/Button";
 import coin from "../../assets/coin.svg";
 import { BuyBox, BuyButtons, Close } from "./Styles";
 
 function BuyPoints({ onAddPoints, setOpenBuy }) {
+  const buyPointsRef = useRef();
+
   function onAdd(amount) {
     onAddPoints(amount);
     setOpenBuy(false);
   }
 
+  function handleClickOutside(event) {
+    if (buyPointsRef.current && !buyPointsRef.current.contains(event.target)) {
+      setOpenBuy(false);
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  });
+
   return (
-    <BuyBox>
+    <BuyBox ref={buyPointsRef}>
       <AeroPay />
       <BuyButtons>
         <Button small onClick={() => onAdd(1000)}>
